fa511e310587a0aca405848e4b478c49dd678622
Shipon <mdshiponmolla3@gmail.com>
2024-02-13 13:45:33 +0600

    fb chat

--- components/ui/auth/facebookMsg.tsx
+++ components/ui/auth/facebookMsg.tsx
@@ -1,8 +1,30 @@
 "use client";
 
+import { useEffect } from "react";
 import { CustomChat, FacebookProvider } from "react-facebook";
 
 export const FacebookMsg = () => {
+  useEffect(() => {
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        xfbml: true,
+        version: "v18.0",
+      });
+    };
+
+    (function (d, s, id) {
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) {
+        return;
+      }
+      js = d.createElement(s);
+      js.id = id;
+      //@ts-ignore
+      js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+      fjs.parentNode!.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  }, []);
   return (
     <FacebookProvider appId={`${process.env.NEXT_PUBLIC_APP_ID}`}>
       <CustomChat
